5c472f93addb15400ac64e2e0111e6688b2a92d3
vam-09 <72458993+vam-09@users.noreply.github.com>
2020-12-22 16:02:14 +0530

    Draw and Change Color
    draws circle or rectangle according to our choice and can change color of then after mouseclick

---
+++ Draw and Change color/app.ts
@@ -0,0 +1,80 @@
+var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
+var context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+//here we are adding event
+canvas.addEventListener("click",mouseclick,false);
+
+
+//input element radius of circle & length if side of square
+var t1: HTMLInputElement = <HTMLInputElement> document.getElementById('t1');
+var t2: HTMLInputElement = <HTMLInputElement> document.getElementById('t2');
+var t3: HTMLInputElement = <HTMLInputElement> document.getElementById('t3');
+
+//boundry of canvas
+var rect=canvas.getBoundingClientRect();
+//to obtain plain canvas
+var geo:string="none";
+//container in which we can place onjects
+var circlecontainer:{circle:Circle}[]=[];
+//var sqcontainer:{rect:Square}[]=[];
+var reccontainer:{rec:Rectangle}[]=[];
+
+//function to draw circle
+function drawcircle(){
+    geo="circle"
+    }
+//function to draw rectangle
+function drawrect(){
+    geo="rect"
+    }
+//function to change color
+function changecolor(){
+    geo="changecolor"
+    }
+//mouseclick function
+function mouseclick(e: MouseEvent)
+{
+    //for circle
+   if(geo=="circle"){
+   let c1=new Point(e.clientX-rect.left,e.clientY-rect.top);
+   let cir1=new Circle(c1,+t1.value,context);
+   cir1.draw();
+   circlecontainer.push({circle:cir1})
+   }
+   //for rectangle
+   else if(geo=="rect"){
+   let c1=new Point(e.clientX-rect.left,e.clientY-rect.top);
+   let rec=new Rectangle(c1,+t2.value,+t3.value,context);
+   rec.draw();
+   reccontainer.push({rec :rec})
+   }
+   //for changing color
+   else if(geo=="changecolor"){
+       for(let i=0;i<circlecontainer.length;i++){
+           //to check clicking point is inside or outside of circle
+           if(circlecontainer[i].circle.isinside(new Point(e.clientX-rect.left,e.clientY-rect.top))){
+               //to set color
+               circlecontainer[i].circle.color="yellow";
+               break;
+           }
+       }
+        //context.clearRect(0,0,canvas.width,canvas.height);----------------//to clear rectangles
+        for(let i=0;i<circlecontainer.length;i++){
+            circlecontainer[i].circle.draw();               //to draw circle
+        }
+
+        //for square
+        for(let j=0;j<reccontainer.length;j++){
+            //to check clicking point is inside or outside of square
+            if(reccontainer[j].rec.isinside(new Point(e.clientX-rect.left,e.clientY-rect.top))){
+                //to set color
+                reccontainer[j].rec.color="yellow";
+                break;
+            }
+        }
+         //context.clearRect(0,0,canvas.width,canvas.height);----------------//to clear rectangles
+         for(let j=0;j<reccontainer.length;j++){
+            reccontainer[j].rec.draw();               //to draw circle
+         }
+   }
+}
---
+++ Draw and Change color/framework.ts
@@ -0,0 +1,87 @@
+class Point{
+    public x:number;
+    public y:number;
+    constructor(x:number,y:number){
+       this.x=x;
+       this.y=y;
+    }
+}
+class Circle{
+    private cenpt:Point;
+    private radius:number;
+    private context:CanvasRenderingContext2D;
+    private _color:string="red";
+    constructor(cenpt:Point,radius:number,context:CanvasRenderingContext2D){
+       this.cenpt=cenpt;
+       this.radius=radius;
+       this.context=context;
+
+    }
+
+    draw()
+    {
+        this.context.beginPath();
+        this.context.arc(this.cenpt.x,this.cenpt.y, this.radius,0,2*Math.PI,false)
+        this.context.fillStyle= this._color;
+        //console.log(this._color)
+        this.context.fill();
+        this.context.strokeStyle= "black";  
+        this.context.stroke();
+    }
+    //to set color
+    set color(value:string){
+        this._color=value;
+    }
+    //to set center point
+    set cenpoint(pt:Point){
+        this.cenpt=pt;
+    }
+
+
+    isinside(pt:Point):boolean{
+        let r=Math.sqrt(Math.pow(pt.x-this.cenpt.x,2)+Math.pow(pt.y-this.cenpt.y,2));
+        if(r<this.radius){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+}
+class Rectangle{
+    private stpt:Point;
+    private length:number;
+    private height:number;
+    private _color:string="blue";
+    private context:CanvasRenderingContext2D;
+    constructor(stpt:Point,length:number,height:number,context:CanvasRenderingContext2D){
+       this.stpt=stpt;
+       this.length=length;
+       this.height=height;
+       this.context=context;
+    }
+
+    draw()
+    {
+         this.context.beginPath();
+        this.context.rect(this.stpt.x,this.stpt.y, this.length,this.height)
+        this.context.fillStyle= this._color;
+        this.context.strokeStyle= "black";
+        this.context.fill();
+        this.context.stroke();
+    }
+    set color(value:string){
+        this._color=value;
+    }
+
+    set stpoint(pt:Point){
+        this.stpt=pt;
+    }
+     
+    isinside(pt:Point):boolean{
+
+    if(pt.x>this.stpt.x-this.length && pt.x<this.stpt.x+this.length && pt.y>this.stpt.y-this.length && pt.y<this.stpt.y+this.length){
+        return true
+    }
+    }
+}
